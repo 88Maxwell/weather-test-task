e3566063f7991890cd4588fb894047828aeb3182
Maxwell <maxwell88soltyk@gmail.com>
2020-08-05 21:59:23 +0300

    Move to hash history

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import "semantic-ui-css/semantic.min.css";
 import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
+import { createHashHistory } from "history";
 
 import App from "./components/Common/App";
 import configureStore from "./domens/Common/configureStore";
 
-const history = createBrowserHistory();
+const history = createHashHistory({ hashType: "slash" });
 const store = configureStore({ history });
 
 ReactDOM.render(<App store={store} history={history} />, document.getElementById("root"));
